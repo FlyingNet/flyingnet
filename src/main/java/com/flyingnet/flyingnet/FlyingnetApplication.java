067feb7390c82c44f0341c50c2ac434a72f20956
wangjun6 <wangjun6@yusys.com.cn>
2018-11-24 15:03:35 +0800

    init

---
+++ src/main/java/com/flyingnet/flyingnet/FlyingnetApplication.java
@@ -0,0 +1,12 @@
+package com.flyingnet.flyingnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyingnetApplication.class, args);
+    }
+}
---
+++ src/test/java/com/flyingnet/flyingnet/FlyingnetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flyingnet.flyingnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyingnetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
